57b739e66162b12b4103ee994945f2ac6a593935
Shevik11 <maksshevchuk621@gmail.com>
2025-01-11 22:15:33 +0200

    make viewsets

--- fapp/views.py
+++ fapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from fapp.models import Flight, Passenger, Reservation
+from .serializers import FlightSerializer, PassengerSerializer, ReservationSerializer
+from rest_framework import viewsets
 # Create your views here.
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+class PassengerViewSet(viewsets.ModelViewSet):
+    queryset = Passenger.objects.all()
+    serializer_class = PassengerSerializer
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+def display(request):
+    return HttpResponse("Hello, World!")
+
